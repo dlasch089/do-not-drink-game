0b9bae9196ceba035c57ec6576842f555a1669dd
Dominik Lasch <dl@exutec.de>
2017-10-27 13:30:24 +0200

    first part in Javascript // Equivalent JQuery

--- js/javascript.js
+++ js/javascript.js
@@ -2,10 +2,41 @@
 
 $(document).ready(function() {
 
+  startScreen();
 
-  $('h1').click(function() {
-    $('h1').hide();
+
+  console.log('Validated!');
+});
+
+function startScreen() {
+  // ----------- DONE IN PURE JS DOM ------------------ //
+  var main = document.getElementById('main');
+  var divAround = document.createElement('div');
+  main.appendChild(divAround);
+  var text = document.createElement('h1');
+  text.innerHTML = 'Do The Opposite!'.toUpperCase();
+  text.setAttribute('class', 'padding-startscreen');
+  var startButton = document.createElement('button');
+  startButton.innerHTML = "Let's Go!".toUpperCase();
+  startButton.setAttribute('class', 'button');
+  startButton.setAttribute('id', 'ghost-btn');
+
+  divAround.appendChild(text);
+  divAround.appendChild(startButton);
+
+  startButton.addEventListener('click', function() {
+    $('#ghost-btn').hide();
   });
 
+  // $('#ghost-btn').click(function() {
+  //   $('#ghost-btn').hide();
+  //   var game = new CreateGame();
+  // });
 
-});
+}
+
+function CreateGame()  {
+  this.challenge = [];
+  this.timeOut = 3;
+  this.score = 0;
+}
